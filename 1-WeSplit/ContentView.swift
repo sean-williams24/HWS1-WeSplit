755c581e7c7482101e85e9dfcae2664f88d25223
Sean-Williamsgit config --global user.email sean.r.williams85@gmail.comgit config --global user.name Sean-Williams <wilmslo@gmail.com>
2020-09-03 16:26:51 +0100

    Initial Commit

---
+++ 1-WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  1-WeSplit
+//
+//  Created by Sean Williams on 03/09/2020.
+//  Copyright © 2020 Sean Williams. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
