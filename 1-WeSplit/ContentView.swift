d6c588e3fc76fe2db22f4c17e90cab0bd761c2a7
Sean-Williamsgit config --global user.email sean.r.williams85@gmail.comgit config --global user.name Sean-Williams <wilmslo@gmail.com>
2020-09-04 15:52:05 +0100

    feat: create properties and textfield

--- 1-WeSplit/ContentView.swift
+++ 1-WeSplit/ContentView.swift
@@ -9,13 +9,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var checkAmount = ""
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 2
+
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
     var body: some View {
-        Text("Hello, World!")
+        Form {
+            Section {
+                TextField("Amount", text: $checkAmount)
+                    .keyboardType(.numberPad)
+
+            }
+            
+            Section {
+                Text("$\(checkAmount)")
+            }
+        }
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+
     }
 }
