a39119ed09384e2e28b1f57c8a19bbc76bbd9f3a
Sean-Williamsgit config --global user.email sean.r.williams85@gmail.comgit config --global user.name Sean-Williams <wilmslo@gmail.com>
2020-09-04 16:23:37 +0100

    feat:  Add textfield, Picker, segmented picker and textview

--- 1-WeSplit/ContentView.swift
+++ 1-WeSplit/ContentView.swift
@@ -11,22 +11,39 @@ import SwiftUI
 struct ContentView: View {
     
     @State private var checkAmount = ""
-    @State private var numberOfPeople = 2
+    @State private var numberOfPeople = 0
     @State private var tipPercentage = 2
 
     let tipPercentages = [10, 15, 20, 25, 0]
     
     var body: some View {
-        Form {
-            Section {
-                TextField("Amount", text: $checkAmount)
-                    .keyboardType(.numberPad)
-
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", text: $checkAmount)
+                        .keyboardType(.numberPad)
+                }
+                
+                Picker("Number of people", selection: $numberOfPeople) {
+                    ForEach(2 ..< 99) {
+                        Text("\($0) people")
+                    }
+                }
+                
+                Section(header: Text("How much tip would you liek to leave?")) {
+                    Picker("Tip Percentage", selection: $tipPercentage) {
+                        ForEach(0 ..< tipPercentages.count) {
+                            Text("\(self.tipPercentages[$0])%")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+                
+                Section {
+                    Text("$\(checkAmount)")
+                }
             }
-            
-            Section {
-                Text("$\(checkAmount)")
-            }
+        .navigationBarTitle("WeSplit")
         }
     }
 }
